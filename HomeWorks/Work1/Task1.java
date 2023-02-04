4294c060d2c716886399a6734f014964b14f6a76
MaksWoropaew <maks.woropaew@mail.ru>
2023-02-04 14:37:12 +0300

    addhw2

---
+++ HomeWorks/Work1/Task1.java
@@ -0,0 +1,24 @@
+package HomeWorks.Work1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // Реализуйте 3 метода, чтобы в каждом из них получить разные исключения
+
+        addOne(1);
+        createNegativeLenght(-1);
+        StringToInt("");
+
+    }
+
+    public static int addOne(int x) {
+        return addOne(x + 1);
+    }
+
+    public static int[] createNegativeLenght(int size) {
+        return new int[size];
+    }
+
+    public static int StringToInt(String input) {
+        return Integer.parseInt(input);
+    }
+}
---
+++ HomeWorks/Work2/task1.java
@@ -0,0 +1,27 @@
+package HomeWorks.Work2;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        // Реализуйте метод, который запрашивает у пользователя ввод дробного числа
+        // (типа float), и возвращает введенное значение.
+        // Ввод текста вместо числа не должно приводить к падению приложения,
+        // вместо этого, необходимо повторно запросить у пользователя ввод данных.
+        System.out.println(getFloat());
+    }
+
+    public static float getFloat() {
+        float input = 0f;
+        while (true) {
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.println("Введите дробное число: ");
+                if (scanner.hasNextFloat()) {
+                    input =  scanner.nextFloat();
+                    break;
+                }
+            }
+        }
+        return input;
+    }
+}
