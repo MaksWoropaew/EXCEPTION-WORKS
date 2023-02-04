aeb71608e5539e9a6d11fa4a4444aea542842201
MaksWoropaew <maks.woropaew@mail.ru>
2023-02-04 14:46:39 +0300

    add less3

---
+++ Lections/less3/ByZeroExeption.java
@@ -0,0 +1,8 @@
+package Lections.less3;
+
+public class ByZeroExeption extends ArithmeticException {
+    public ByZeroExeption() {
+        super("Ошибка, произошло деление на ноль");
+
+    }
+}
---
+++ Lections/less3/task2.java
@@ -0,0 +1,24 @@
+package Lections.less3;
+import java.io.IOException;
+
+public class task2 {
+    public static void main(String[] args) {
+        // Создайте класс Счетчик, у которого есть метод add(),
+        // увеличивающий значение внутренней int переменной на 1.
+        // Сделайте так, чтобы с объектом такого типа можно было работать в блоке
+        // try-with-resources.
+        // Нужно бросить исключение, если работа с объектом типа счетчик была не в
+        // ресурсном try и/или ресурс остался открыт.
+        // Подумайте какой тип исключения подойдет лучше всего.
+        Counter counter = new Counter(2);
+        counter.add();
+        try (counter) {
+            counter.add();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //counter.add();
+        System.out.println(counter.getCount());
+    }
+}
